e2187a2113f4bd35ad4d0bb68a8ee6f555368d09
SarekSJ <Syke117@gmail.com>
2018-03-14 22:38:34 -0700

    Add id collection

--- SpotifyStuff.py
+++ SpotifyStuff.py
@@ -15,12 +15,29 @@ jazz10000 = '2hJBSU1hLZUVDI9WT37pmI'
 rock10000 = '3PO7Cfl1eoMcjhnq1IwByd'
 hiphop10000 = '4ga5O625dPrhIp7G6i71y8'
 
-# playlists = [jazz10000, rock10000, hiphop10000]
-playlists = [jazz10000]
-for playlist in playlists:
+woop = '1Zyxj9mEn9EI6t0RfYGDTY' # For testing
+
+playlists = [jazz10000, rock10000, hiphop10000]
+playlist_names = ['jazz', 'rock', 'hip_hop']
+# playlists = [jazz10000]
+for index, playlist in enumerate(playlists):
+    print('Doing ' + playlist_names[index] + '...')
     uri = 'spotify:user:' + username + ':playlist:' + playlist
     results = sp.user_playlist(username, playlist, fields='tracks')
-    for track in results['tracks']['items']:
-        print(track['track']['id'])
-    # pprint(tracks)
+    # print(results['tracks'])
+    with open(playlist_names[index] + '_ids.txt', 'w') as f:
+        for track in results['tracks']['items']:
+            # print(track['track']['id'])
+            f.write(track['track']['uri'] + '\n')
+        results = sp.next(results['tracks'])
+        while (results['next']):
+            for track in results['items']:
+                # pprint(track['track']['id'])
+                f.write(track['track']['uri'] + '\n')
+            # print(results['next'])
+            try:
+                results = sp.next(results)
+
+            except:
+                break
 

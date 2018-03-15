3f868f491f71d840b81c85692d19e419b8ee1aa1
SarekSJ <Syke117@gmail.com>
2018-03-15 11:44:21 -0700

    add mml stuff

--- mlstuff.py
+++ mlstuff.py
@@ -11,7 +11,7 @@ from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
 from sklearn import preprocessing
 from pprint import pprint
-# danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration_ms, time_signature
+# danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, time_signature
 
 genres = {0:'jazz', 1:'rock', 2:'hip_hop'}
 genre_x = [[] , [] , []]
@@ -23,6 +23,9 @@ genre_y_train = []
 genre_x_test = []
 genre_y_test = []
 
+plot_yes = True
+
+
 def get_attributes():
     for key, value in genres.items():
         tibs = []
@@ -32,15 +35,33 @@ def get_attributes():
                 attributes = line.split(',')
                 genre_x_train.append([float(x) for x in attributes[:len(attributes)-1]])
                 genre_y_train.append(int(attributes[len(attributes)-1]))
-            pprint (len(genre_x_train))
+            # pprint (len(genre_x_train))
 
-        with open('analysis_' + value + '_ids_train.txt', 'r') as f:
+        with open('analysis_' + value + '_ids_test.txt', 'r') as f:
             for line in f:
                 attributes = line.split(',')
                 genre_x_test.append([float(x) for x in attributes[:len(attributes)-1]])
                 genre_y_test.append(int(attributes[len(attributes)-1]))
-            pprint (len(genre_x_train))
+            # pprint (len(genre_x_train))
 
+def plot_errors(new_y, type):
+    x_errors = []
+    y_errors = []
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+    if plot_yes:
+
+        for index, y in enumerate(new_y):
+            if y != genre_y_test[index]:
+                x_errors.append(genre_x_test[index])
+                y_errors.append(y)
+        x_errors = np.array(x_errors)
+        y_errors = np.array(y_errors)
+        plt.scatter(x_errors[:, 0], x_errors[:, 1], c=y_errors, cmap=cmap_light,
+                    edgecolor='k', s=20)
+        plt.xlabel("Dancebility")
+        plt.ylabel("Energy")
+        plt.title("Errors for : " + type)
+        plt.show()
 
 def plot_pca(X, y):
     np.random.seed(5)
@@ -80,8 +101,15 @@ def knn_plot(X, y):
     n_neighbors = math.sqrt(len(X))
     knn = KNeighborsClassifier()
     X_r2 = knn.fit(X, y)
+    print('Knn: ')
+
+    new_y = knn.predict(X)
+    print('Train Error: ' + str(compare_y(new_y, y)))
+
+
     new_y = knn.predict(genre_x_test)
-    compare_y(new_y, genre_y_test)
+    print('Test Error: ' + str(compare_y(new_y, genre_y_test)))
+    # plot_errors(new_y, "Knn")
 
     h = 0.02
 
@@ -91,10 +119,12 @@ def knn_plot(X, y):
 
     target_names = ['jazz','rock','hip_hop']
     colors = ['navy', 'turquoise', 'darkorange']
-    # x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    # y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    # x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    # y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
     # xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
     #                      np.arange(y_min, y_max, h))
+    # Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
+
 
     plt.figure()
     # plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
@@ -102,10 +132,13 @@ def knn_plot(X, y):
     # Plot also the training points
 
     plt.scatter(genre_x_test[:, 0], genre_x_test[:, 1], c=new_y, cmap=cmap_light,
-                edgecolor='k', s=20)
+                edgecolor='k', s=10)
+    plt.xlabel("Dancebility")
+    plt.ylabel("Energy")
     # plt.xlim(xx.min(), xx.max())
     # plt.ylim(yy.min(), yy.max())
-    plt.title("3-Class classification using KNN")
+    plt.title("KNN for the Music Dataset")
+    plt.show()
 
 
 
@@ -116,33 +149,78 @@ def plot_lda(X, y):
     # y = iris.target
 
     # print(type(X))
+
+    print('LDA: ')
+
     target_names = ['jazz','rock','hip_hop']
 
     lda = LinearDiscriminantAnalysis(n_components=12)
     X_r2 = lda.fit(X, y).transform(X)
-    new_y = lda.predict(genre_x_test)
 
-    compare_y(new_y, genre_y_test)
+    new_y = lda.predict(X)
+    print("Train error rate: " + str(compare_y(new_y, y)))
+
+    new_y = lda.predict(genre_x_test)
+    print("Test error rate: " + str(compare_y(new_y, genre_y_test)))
+    plot_errors(new_y, "LDA")
 
     colors = ['navy', 'turquoise', 'darkorange']
     plt.figure()
     for color, i, target_name in zip(colors, [0, 1, 2], target_names):
         plt.scatter(X_r2[y == i, 0], X_r2[y == i, 1], alpha=.8, color=color,
-                    label=target_name)
+                    label=target_name, s=0.5)
     plt.legend(loc='best', shadow=False, scatterpoints=1)
     plt.title('LDA of music dataset')
+    plt.xlabel("Dancebility")
+    plt.ylabel("Energy")
+    plt.show()
+
+def plot_qda(X, y):
+    print('QDA: ')
+    qda = QuadraticDiscriminantAnalysis()
+    qda.fit(X,y)
+
+    new_y = qda.predict(X)
+    print("Train error rate: " + str(compare_y(new_y, y)))
+
+    new_y = qda.predict(genre_x_test)
+    print("Test error rate: " + str(compare_y(new_y, genre_y_test)))
+
+    plot_errors(new_y, "QDA")
+
+    colors = ['navy', 'turquoise', 'darkorange']
+    target_names = ['jazz','rock','hip_hop']
+
+    plt.figure()
+    for color, i, target_name in zip(colors, [0, 1, 2], target_names):
+        plt.scatter(X[y == i, 0], X[y == i, 1], alpha=.8, color=color,
+                    label=target_name, s=0.5)
+    plt.legend(loc='best', shadow=False, scatterpoints=1)
+    plt.title('QDA of music dataset')
+    plt.xlabel("Dancebility")
+    plt.ylabel("Energy")
     plt.show()
 
 
 def logistic_regression(X, y):
+    print('Logistic Regression: ')
     h = .02  # step size in the mesh
 
     logreg = linear_model.LogisticRegression(C=1e5)
 
     # we create an instance of Neighbours Classifier and fit the data.
     result = logreg.fit(X, y)
+
+    new_y = logreg.predict(X)
+    error_rate = compare_y(new_y,y)
+    print("Train error rate: " + str(error_rate))
+
+
     new_y = logreg.predict(genre_x_test)
-    compare_y(new_y,genre_y_test)
+    error_rate = compare_y(new_y,genre_y_test)
+    print("Test error rate: " + str(error_rate))
+    # plot_errors(new_y, "Logistic Regression")
+
 
     # Plot the decision boundary. For that, we will assign a color to each
     # point in the mesh [x_min, x_max]x[y_min, y_max].
@@ -153,13 +231,15 @@ def logistic_regression(X, y):
 
     # Plot also the training points
     plt.scatter(genre_x_test[:, 0], genre_x_test[:, 1], c=new_y, cmap=cmap_light,
-                edgecolor='k', s=20)
-    # plt.xlim(xx.min(), xx.max())
-    # plt.ylim(yy.min(), yy.max())
-    plt.title("3-Class classification using Logistic Regression")
-    plt.xticks(())
-    plt.yticks(())
-
+                edgecolor='k', s=10)
+    plt.xlabel("Dancebility")
+    plt.ylabel("Energy")
+    # # plt.xlim(xx.min(), xx.max())
+    # # plt.ylim(yy.min(), yy.max())
+    plt.title("Logistic Regression for the Music Dataset")
+    # plt.xticks(())
+    # plt.yticks(())
+    #
     plt.show()
 
 def compare_y(new_y, old_y):
@@ -169,7 +249,7 @@ def compare_y(new_y, old_y):
         if y != old_y[index]:
             errors+=1
         count+=1
-    print(errors/count)
+    # print(errors/count)
     return (errors/count)
 
 # plot_qda()
@@ -180,15 +260,11 @@ genre_x_test = np.array(genre_x_test)
 genre_y_test = np.array(genre_y_test)
 
 
-# plot_pca(genre_x_train, genre_y_train)
-scaler = preprocessing.MinMaxScaler()
+genre_x_test = preprocessing.scale(genre_x_test)
+genre_x_train = preprocessing.scale(genre_x_train)
+# #
+# plot_lda(genre_x_train, genre_y_train)
+# plot_qda(genre_x_train, genre_y_train)
 
-genre_x_train = scaler.fit_transform(genre_x_train, genre_y_train)
-genre_x_test = scaler.fit_transform(genre_x_test, genre_y_test)
-
-# genre_x_test = preprocessing.scale(genre_x_test)
-# genre_x_train = preprocessing.scale(genre_x_train)
-
-plot_lda(genre_x_train, genre_y_train)
 knn_plot(genre_x_train, genre_y_train)
 logistic_regression(genre_x_train, genre_y_train)
